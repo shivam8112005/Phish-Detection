3abea109eabc2befc93a021780bb5537f26dc407
shivam shukla <shivamshukla8112005@gmail.com>
2025-08-11 14:23:29 +0530

    Extracted text from the files in /scam api endpoint

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request
+import google.generativeai as genai
+import os
+import PyPDF2
+
 
 #app
 app = Flask(__name__)
@@ -12,8 +16,19 @@ def index():
 @app.route('/scam/', methods = ['GET', 'POST'])
 def detect_scam():
     if "file" not in request.files:
-        pass
-         
+        return render_template('index.html', message="Please Upload a File !")
+    file = request.files['file']
+    print(file)
+    extracted_text = ""
+
+    if file.filename.endswith('.pdf'):
+        pdf_reader = PyPDF2.PdfReader(file)
+        pdf_text = " ".join([page.extract_text() for page in pdf_reader.pages if page.extract_text()])
+
+    elif file.filename.endswith('.txt'):
+        extracted_text = file.read().decode('utf-8')
+    else:
+        return render_template('index.html', message='Error fetching the text! File is Empty or please Enter .pdf or .txt file only.')       
 
 #python main
 if  __name__ == '__main__':
