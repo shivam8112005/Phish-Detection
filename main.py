3273306d41fa683895311b9e05e946dd362fe267
shivam shukla <shivamshukla8112005@gmail.com>
2025-08-17 19:01:56 +0530

    performed oversampling to balance imbalance classes in ds

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ import google.generativeai as genai
 import os
 import PyPDF2
 from dotenv import load_dotenv
+import pickle
 
 load_dotenv()
 
+
+sms_model_path = 'sms_pipeline.pkl'
+email_model_path = 'email_pipeline.pkl'
+url_model_path = 'url_pipeline (1).pkl'
+with open(sms_model_path, 'rb') as file:
+    sms_model = pickle.load(file) 
+with open(email_model_path, 'rb') as file:
+    email_model = pickle.load(file) 
+with open(url_model_path, 'rb') as file:
+    url_model = pickle.load(file) 
 
 api_key = os.getenv("google_api_key")
 #app
@@ -35,6 +46,33 @@ def detect_scam():
         return render_template('index.html', message='Error fetching the text! File is Empty or please Enter .pdf or .txt file only.')  
     print(extracted_text)
     return render_template('index.html')
+
+
+
+
+@app.route("/url_predict", methods=["POST"])
+def url_predict():
+    if request.method == "POST":
+        url = request.form["url"]
+
+        # ⚡ Here you need to transform the URL same way as you did in training
+        # Example: simple feature (length of URL)
+        # features = [[len(url)]]
+
+        # Predict
+        proba = url_model.predict_proba([url])[0]
+        threshold = 0.4   # make model more sensitive to phishing
+        prediction = 1 if proba[1] >= threshold else 0
+        print("-------------------------------- ",prediction)
+        print("-------------------------------- hjfbwiubwj f",proba)
+        
+        result = "Phishing" if prediction == 1 else "Legitimate"
+        # print(result)
+
+        return render_template("index.html", url_result=result, url_entered=url)
+
+
+
 
 #python main
 if  __name__ == '__main__':
